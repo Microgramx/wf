28dcf09ba8e7b3605ad5dddd681ccb4f08c8bcb8
Microgramsss <geral@webproject.pt>
2024-05-23 23:42:35 +0200

    Adicionando todos os arquivos do projeto

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyA4BrhBJdOBnksVnoYlLRd3kzyukvJIULw",
+    authDomain: "hdscreen-b299a.firebaseapp.com",
+    projectId: "hdscreen-b299a",
+    storageBucket: "hdscreen-b299a.appspot.com",
+    messagingSenderId: "548970071246",
+    appId: "1:548970071246:web:1e026b0bae867edec9bc9f",
+    measurementId: "G-NC6473VX1R"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+// Initialize the Firestore database
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
+export default firebaseApp;
